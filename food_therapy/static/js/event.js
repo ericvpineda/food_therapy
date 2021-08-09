808539eea4dc0be4e6c36d69fa80f19fb946bedc
Tram Nguyen <tramn@berkeley.edu>
2021-08-09 13:51:42 -0700

    Chat function done

---
+++ food_therapy/static/js/event.js
@@ -0,0 +1,81 @@
+var me = {};
+me.avatar = "https://i.ibb.co/0s74kVh/zac-m-wright-healthcare-consultant-avatar-Head-tilt-April-2020.png";
+
+var you = {};
+you.avatar = "https://i.ibb.co/QjnzbK0/unnamed.png";
+
+function formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+}            
+
+//-- No use time. It is a javaScript effect.
+function insertChat(who, text, time){
+    if (time === undefined){
+        time = 0;
+    }
+    var control = "";
+    var date = formatAMPM(new Date());
+    
+    if (who == "me"){
+        control = '<li style="width:100%">' +
+                        '<div class="msj macro">' +
+                        '<div class="avatar"><img class="img-circle" style="width:100%;" src="'+ me.avatar +'" /></div>' +
+                            '<div class="text text-l">' +
+                                '<p>'+ text +'</p>' +
+                                '<p style="color:white;"><small>'+date+'</small></p>' +
+                            '</div>' +
+                        '</div>' +
+                    '</li>';                    
+    }else{
+        control = '<li style="width:100%;">' +
+                        '<div class="msj-rta macro">' +
+                            '<div class="text text-r">' +
+                                '<p>'+text+'</p>' +
+                                '<p><small>'+date+'</small></p>' +
+                            '</div>' +
+                        '<div class="avatar" style="padding:0px 0px 0px 10px !important"><img class="img-circle" style="width:100%;" src="'+you.avatar+'" /></div>' +                                
+                  '</li>';
+    }
+    setTimeout(
+        function(){                        
+            $("ul").append(control).scrollTop($("ul").prop('scrollHeight'));
+        }, time);
+    
+}
+
+function resetChat(){
+    $("ul").empty();
+}
+
+$(".mytext").on("keydown", function(e){
+    if (e.which == 13){
+        var text = $(this).val();
+        if (text !== ""){
+            insertChat("me", text);              
+            $(this).val('');
+        }
+    }
+});
+
+$('body > div > div > div:nth-child(2) > span').click(function(){
+    $(".mytext").trigger({type: 'keydown', which: 13, keyCode: 13});
+})
+
+//-- Clear Chat
+resetChat();
+
+//-- Print Messages
+insertChat("me", "I’m so sorry to hear about your relationship issues!", 0);  
+insertChat("you", "Yeah, it’s been really hard these past few weeks...", 1500);
+insertChat("me", "You gotta try these Chinese Tea boiled eggs, they’re amazing comfort food!", 3500);
+insertChat("you", "I’ll definitely give it a try! I heard about them for a while now.",7000);
+
+
+//-- NOTE: No use time on insertChat.
